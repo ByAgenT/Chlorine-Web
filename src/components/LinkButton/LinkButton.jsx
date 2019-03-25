6cc4533726d3383ba474f0d5a06c6e1cf6472410
Anton Kovalyov <thebyagent@gmail.com>
2019-03-25 12:32:13 +0300

    Add LinkButton component.

---
+++ src/components/LinkButton/LinkButton.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const LinkButton = styled.a`
+  user-select: none;
+  color: white;
+  font-size: 1.15rem;
+  font-weight: 600;
+  margin-left: 1rem;
+  margin-right: 1rem;
+
+  text-decoration: none;
+
+  &:hover {
+    color: rgb(29, 185, 84);
+  }
+`;
+
+export default LinkButton;
